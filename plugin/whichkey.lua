08c06abbc324f35dc4f5d56cea0c5a9b14b8d668
Petzinger, Bryan <bryan.petzinger.civ@army.mil>
2026-08-13 11:33:31 -0400

    add whichkey group labels

--- plugin/whichkey.lua
+++ plugin/whichkey.lua
@@ -2,3 +2,15 @@ local wk = require("which-key")
 wk.setup({
 	preset = "helix",
 })
+
+wk.add({
+	{ "<leader>b", group = "Buffer" },
+	{ "<leader>c", group = "Code" },
+	{ "<leader>d", group = "Debug / Profiler" },
+	{ "<leader>f", group = "Find" },
+	{ "<leader>g", group = "Git" },
+	{ "<leader>q", group = "Session" },
+	{ "<leader>s", group = "Search" },
+	{ "<leader>u", group = "UI / Toggles" },
+	{ "<leader>w", group = "Window" },
+})
